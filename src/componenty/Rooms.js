d05e4c39708442b4a69bc1b16bcbaffd89368377
Carrbek <rochkonopacki@gmail.com>
2024-06-07 03:39:46 +0200

    Update Rooms.js

--- src/componenty/Rooms.js
+++ src/componenty/Rooms.js
@@ -15,7 +15,7 @@ const Rooms = () => {
       <div className='lg:top-20 relative lg:col-span-1 col-span-2'>
         <h3 className='text-2xl font-bold'>Poznaj zasady bezpieczeństwa</h3>
         <p className='text-xl py-6'>
-          Zapoznaj się z naszym systemem szkoleniowym. Regularnie organizujemy szkolenia dla producentów i właścicieli obiektów sportowych i rekreacyjnych. Nasze szkolenia nie tylko dostarczają wiedzy teoretycznej, ale także umożliwiają praktyczne wykorzystanie zdobytej wiedzy. Naszym celem jest wykształcenie profesjonalistów, którzy nie tylko rozumieją teorię, ale także potrafią skutecznie działać w praktyce, przyczyniając się do podnoszenia standardów bezpieczeństwa w branży.
+          Zapoznaj się z naszym systemem szkoleniowym. Regularnie organizujemy szkolenia dla producentów i właścicieli obiektów sportowych <br/> i rekreacyjnych. Nasze szkolenia nie tylko dostarczają wiedzy teoretycznej, ale także umożliwiają praktyczne wykorzystanie zdobytej wiedzy. Naszym celem jest wykształcenie profesjonalistów, którzy nie tylko rozumieją teorię, ale także potrafią skutecznie działać w praktyce, przyczyniając się do podnoszenia standardów bezpieczeństwa w branży.
         </p>
         <div className="text-center">
           <button className='ml-24 text-center border-black mr-4 hover:shadow-xl' onClick={handleClick}>
